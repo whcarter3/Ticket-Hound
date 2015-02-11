8d3470994316f8fd9ee4b08826509bb4246566c1
Will Carter <whcarter3@gmail.com>
2015-02-11 07:32:28 -0800

    user validations

--- rails-backend/app/models/user.rb
+++ rails-backend/app/models/user.rb
@@ -2,10 +2,13 @@ class User
   include Mongoid::Document
   include ActiveModel::SecurePassword
 
-  validates_uniqueness_of :username, :email { case_sensitive: false }
-  validates_presence_of :username, :email, :password, :password_confirmation
+  validates :email, 
+  			uniqueness: {case_sensitive: false}, 
+  			presence: true
+  validates_uniqueness_of :username
+  validates_presence_of :username, :password, :password_confirmation
   validates_length_of :password, minimum: 8, maximum: 24, too_long: "Max password length is 24 characters", too_short: "Minimum password length is 8 characters"
-  validates_format_of :title, with: \b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b
+  validates_format_of :title, :with \b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b
 
   field :username, type: String
   field :email, type: String
