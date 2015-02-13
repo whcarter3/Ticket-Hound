349a15f5293565034eeb73a592a00bb03dba5b89
garrettrh <garrett.harris@gmail.com>
2015-02-12 22:43:49 -0800

    Add rails configurations.

--- rails-backend/config/routes.rb
+++ rails-backend/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  # return 200 for OPTION requests (XSS, Test Only)
+  match '*all' => 'application#handle_options', via: :options
+
   root 'home#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
