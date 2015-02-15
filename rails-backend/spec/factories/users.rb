962ba6263b66b135f0e526bd621acf26fc68a9aa
Will Carter <whcarter3@gmail.com>
2015-02-15 10:31:37 -0800

    moved rails-backend to root

--- rails-backend/spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    username              "testuser"
-    email                 "test@test.com"
-    password              "password"
-    password_confirmation "password"
-  end
-end
