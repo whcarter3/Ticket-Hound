140d4eb23581598587651e1afcd6b3eee1fd1b44
Will Carter <whcarter3@gmail.com>
2015-02-15 17:06:44 -0800

    modified user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,17 +16,42 @@ RSpec.describe User, :type => :model do
   end
 
   it "is invalid without a username" do
-    no_username = FactoryGirl.build(:user, username: nil)
-    expect(no_username).to be_invalid
+    user_empty_field = FactoryGirl.build(:user, username: nil)
+    expect(user_empty_field).to be_invalid
+  end
+
+  it "is invalid if username is not unique" do
+    user2  = FactoryGirl.build(:user, username: "bob")
+    user3  = FactoryGirl.build(:user, username: "Bob")
+    expect(user2).to be_invalid
+    expect(user3).to be_invalid
+  end
+
+  it "is invalid if username is less than 3 letters" do
+    user_shortname = FactoryGirl.build(:user, username: 'al')
+    expect(user_shortname).to be_invalid
   end
 
   it "is invalid without an email" do
-    no_username = FactoryGirl.build(:user, email: nil)
-    expect(no_username).to be_invalid
+    user_empty_field = FactoryGirl.build(:user, email: nil)
+    expect(user_empty_field).to be_invalid
+  end
+
+  it "is invalid without a unique email" do
+    user = FactoryGirl.build(:user, email: 'test@test.com')
+    user1 = FactoryGirl.build(:user, email: 'Test@test.com')
+    expect(user).to be_invalid
+    expect(user1).to be_invalid
+  end
+
+  it "is invalid without a properly formatted email" do
+    user_bad_email = FactoryGirl.build(:user, email: 'yaddayadda')
+    expect(user_bad_email).to be_invalid
   end
 
   it "is invalid without a password" do
-    no_username = FactoryGirl.build(:user, password: nil)
-    expect(no_username).to be_invalid
+    user_empty_field = FactoryGirl.build(:user, password: nil)
+    expect(user_empty_field).to be_invalid
   end
+
 end
