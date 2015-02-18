11bbca54c86376d89040a97272398686a3d8a153
garrettrh <garrett.harris@gmail.com>
2015-02-17 22:37:46 -0800

    Add ui router with basic routes for signup and login.

--- angular-frontend/js/app.js
+++ angular-frontend/js/app.js
@@ -1,2 +1,49 @@
 angular
-    .module('app', ['ngResource']);
+    .module('app', ['ngResource', 'ui.router']);
+    .config(MainRouter);
+
+    function MainRouter($stateProvider, $urlRouterProvider){
+
+        $stateProvider
+            .state("main", {
+                url: "/",
+                templateUrl: "main.html"
+            })
+            .state("home", {
+                url: "/home",
+                templateUrl: "home.html"
+            })
+            .state("signup", {
+                url: "/signup",
+                templateUrl: "signup.html"
+            })
+            .state("login", {
+                url: "/login",
+                templateUrl: "login.html"
+            });
+
+        $urlRouterProvider.otherwise("/");
+    }
+
+    //     function MainRouter($stateProvider, $urlRouterProvider){
+
+    //     $stateProvider
+    //         .state("title", {
+    //             url: "/",
+    //             templateUrl: "title.html"
+    //         })
+    //         .state("home", {
+    //             // needs to have url attribute
+    //             url: "/home",
+    //             // and a template url
+    //             templateUrl: "home.html"
+    //         })
+    //         .state("archive", {
+    //             url: "/archive",
+    //             templateUrl: "archive.html"
+    //         });
+
+    //     // like "else" in if/else statement, so it goes back home
+    //     $urlRouterProvider.otherwise("/");
+
+    // }
