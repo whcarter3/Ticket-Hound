1a4fa7795fc241cfba8b9baf328153d46c62dfe6
Will Carter <whcarter3@gmail.com>
2015-02-17 22:30:42 -0800

    added  to controller

--- angular-frontend/js/HoundController.js
+++ angular-frontend/js/HoundController.js
@@ -3,18 +3,39 @@
         .module('app')
         .controller('HoundController', HoundControllerFunc);
 
-        HoundControllerFunc.$inject = ['$resource'];
+        HoundControllerFunc.$inject = ['$resource', '$http'];
 
-        function HoundControllerFunc($resource){
+        function HoundControllerFunc($resource, $http){
             var self = this;
 
             var User = $resource('http://localhost:3000/api/users/:id', {id: '@id'});
+            // var req = {
+            //      method: 'GET',
+            //      url: 'https://app.ticketmaster.com/v1/events?q="gaga"&lat=34.02&long=-118.49&radius=25&apikey=1u3s6SztBBko48JPpTCASNtS0irCgBAI',
+            //      headers: {
+            //        'Content-Type': 'application/json'
+            //      }
+            //      // data: {response: 'response'}
+            //     }
 
             self.Users = getUsers();
+            // self.results = results;
+
 
             function getUsers(){
                 return User.get();
             }
 
-    };
+            // function getResults(req) {
+            //     $http(req)
+            //         .success(function(data){return data})
+            //         .error(function(){return "you done fucked up son"});
+            // };
+
+            $http.jsonp('https://app.ticketmaster.com/v1/events?q="gaga"&lat=34.02&long=-118.49&radius=25&apikey=1u3s6SztBBko48JPpTCASNtS0irCgBAI')
+            .success(function(response) {
+                self.results = response;
+            });
+        }
+
 })();
