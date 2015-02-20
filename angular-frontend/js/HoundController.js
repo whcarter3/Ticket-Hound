2154a12507f310f4a321c531aa6325e37e161894
garrettrh <garrett.harris@gmail.com>
2015-02-19 19:21:36 -0800

    Parse data on front end.

--- angular-frontend/js/HoundController.js
+++ angular-frontend/js/HoundController.js
@@ -11,26 +11,28 @@
             var User = $resource('http://localhost:3000/api/users/:id', {id: '@id'}, {'update': {method: "PUT"}});
 
             self.Users = getUsers();
-            self.addUser = addUser();
+            // self.addUser = addUser();
             self.results = tixRequest();
             // self.results = ticketMaster();
+            // self.performerName = performerName;
 
             function getUsers(){
                 return User.get();
             }
 
-            function addUser(){
-                newUser = new User();
-                newUser.username = self.text;
-                newUser.email = self.text;
-                newUser.password = self.text;
-                newUser.password_confirm = self.text;
-                newUser.$save();
-            }
+            // function addUser(){
+            //     newUser = new User();
+            //     newUser.username = self.text;
+            //     newUser.email = self.text;
+            //     newUser.password = self.text;
+            //     newUser.password_confirm = self.text;
+            //     newUser.$save();
+            // }
 
 
             function tixRequest(){
-                var url = 'http://jsonpify.heroku.com?q="gaga"&lat=34.02&long=-118.49&radius=25&apikey=1u3s6SztBBko48JPpTCASNtS0irCgBAI&callback=JSON_CALLBACK&resource=https://app.ticketmaster.com/v1/events';
+                var performerName = performerName;
+                var url = 'http://jsonpify.heroku.com?q="' + performerName + '"&lat=34.02&long=-118.49&radius=25&apikey=1u3s6SztBBko48JPpTCASNtS0irCgBAI&callback=JSON_CALLBACK&resource=https://app.ticketmaster.com/v1/events';
                 $http.jsonp(url)
                     .success(function(data, status, headers, config) {
                     console.log(data);
